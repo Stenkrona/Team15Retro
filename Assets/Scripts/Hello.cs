5ddf289da150a2b3f26dab40d78f3b5a171e54f7
quitefranklyfrank <hanna.friden@futuregames.nu>
2019-03-11 15:01:14 +0100

    Breaking stuff

---
+++ Assets/Scripts/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+
+    string myHello = "Hello!";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
