4dc5d03d198e13e1c66c15852b5421f8a0245a57
RobertoTheWizardo <oscar@werbig.com>
2019-03-14 16:19:45 +0100

    Spawner commit updated spawner that spawns in the same maner as before but needs to be called to remove the tetris groups

--- Assets/Code/Spawner.cs
+++ Assets/Code/Spawner.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
 
-    public GameObject[] Groups;
+    public Vector3 playerSpawnpoint;
+    public PlayerInput playerInput;
+
+    public float thrustPower;
+
+    public GameObject[] groups;
+    GameObject instanceGroup;
+
 
     // Start is called before the first frame update
     void Start()
@@ -13,22 +20,41 @@ public class Spawner : MonoBehaviour
         SpawnNext();
     }
 
-    //call this when you want to spawn a random group
+    public void RemoveGroup()
+    {
+        for(int i = 0; i<groups.Length;i++ )
+        {
+            if(instanceGroup == groups[i])
+            {
+                groups[i] = null;
+            }
+        }
+    }
+    
     //can be called using FindObjectOfType<Spawner>().SpawnNext(); OBS only works if the gameobject is called Spawner
     public void SpawnNext()
     {
-        int I = Random.Range(0, Groups.Length);
+        int i = Random.Range(0, groups.Length);
         
-        if(Groups[I] == null)
+        if(groups[i] == null)
         {
             SpawnNext();
             return;
         }
+        
+        if(playerInput != null)
+        {
+            instanceGroup = groups[i];
 
-        //Spawn group at current position
-        Instantiate(Groups[I], transform.position, Quaternion.identity);
+            PlayerController instanceController = Instantiate(groups[i], playerSpawnpoint, Quaternion.identity).AddComponent<PlayerController>();
 
-        Groups[I] = null;
+            instanceController.input = playerInput;
+            instanceController.thrusterPower = thrustPower;
+
+        }
+        else
+        {
+            Debug.Log("NO PLAYER INPUT SELECTED IN SPAWNER");
+        }
     }
-
 }
