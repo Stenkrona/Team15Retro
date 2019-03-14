f70b0950cd0b4448df8d8460a1ea90412ebcf3f1
quitefranklyfrank <hanna.friden@futuregames.nu>
2019-03-14 11:27:32 +0100

    Lägger upp lite välkommenterade scripts.

---
+++ Assets/Code/Mechanics/Landing/TestEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//Don't forget to add the using UnityEngine.Events;
+
+public class TestEvent : MonoBehaviour
+{
+
+    private UnityAction someListener; //We declare the variable for a listener.
+
+    void Awake()
+    {
+        someListener = new UnityAction(SomeFunction);
+    }
+
+
+
+    //Do OnEnable and OnDisable, don't do it on Awake, and Disable before destroying the GameObject.
+    void OnEnable()
+    {
+        EventManager.StartListening("testevent", someListener);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("testevent", someListener);
+    }
+
+    void SomeFunction()
+    {
+        Debug.Log("This is a function which shows us that our Test listener is working.");
+    }
+
+}
