b502cab70eb8fd4bafc936eac938928442be5816
ProfessorFFF <axelstenkrona@me.com>
2019-03-15 15:30:42 +0100

    Added InGameUIManager

---
+++ Assets/Code/UI/InGameUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUIManager : MonoBehaviour
+{
+    private GameStateMachine gameStateMachine_Ref;
+
+    private int playerOneScore;
+    private int playerTwoScore;
+
+    
+    void Start()
+    {
+        gameStateMachine_Ref = GameStateMachine.GetInstance();     
+    }
+    public void UpdateUI()
+    {
+
+    }
+   
+    
+    public int PlayerOneScore { set { playerOneScore = value; } }
+    public int PlayerTwoScore { set { playerTwoScore = value; } }
+}
