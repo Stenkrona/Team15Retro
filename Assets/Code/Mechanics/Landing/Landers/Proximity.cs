2548a6b6de7133f6790ee5ea0cd1731d8367237a
quitefranklyfrank <hanna.friden@futuregames.nu>
2019-03-20 17:05:32 +0100

    Working on an outline that reacts to proximity.

---
+++ Assets/Code/Mechanics/Landing/Landers/Proximity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proximity : MonoBehaviour
+{
+    SpriteRenderer greenGlowRenderer;
+
+    public void Start()
+    {
+       
+        greenGlowRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        //100% transparent
+        greenGlowRenderer.color = new Color(1f, 1f, 1f, 0f);
+
+        //GameObject blockTypeL = gameObject.GetComponent<MyBlockType>;
+        GameObject blockT = GameObject.FindGameObjectWithTag("blockT");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<MyBlockType>().myBlockType == BlockType.T)
+
+        {
+            //50% transparent
+            greenGlowRenderer.color = new Color(1f, 1f, 1f, .5f);
+            //greenGlowRenderer.color = new Color(1f,1f,1f,1f) <- normal
+        }
+
+       
+    }
+
+
+
+        private void Update()
+    {
+        
+    }
+
+}
