888dc2574cd82bcbb46ab26b9c38c65d30049d77
quitefranklyfrank <hanna.friden@futuregames.nu>
2019-03-20 18:07:54 +0100

    Removed findgameobjectswithtag.

--- Assets/Code/Mechanics/Landing/Landers/Proximity.cs
+++ Assets/Code/Mechanics/Landing/Landers/Proximity.cs
@@ -6,11 +6,9 @@ public class Proximity : MonoBehaviour
 {
     SpriteRenderer greenGlowRenderer;
     GameObject targetPoint;
-
-    //GameObject blockT = GameObject.FindGameObjectWithTag("blockT");
-   
-
     float distanceToTarget;
+    GameObject myBlock;
+    GameObject myLander;
 
     public void Awake()
     {
@@ -31,19 +29,22 @@ public class Proximity : MonoBehaviour
 
         void Update()
     {
-        GameObject blockT = GameObject.FindGameObjectWithTag("blockt");
-        distanceToTarget = Vector2.Distance(blockT.transform.position, targetPoint.transform.position);
 
-        if (distanceToTarget < 7 && distanceToTarget > 3)
+        if (myBlock != null && myLander != null)
         {
-            Debug.Log("You are close!");
-            greenGlowRenderer.color = new Color(1f, 1f, 1f, .5f);
-        }
+            distanceToTarget = Vector2.Distance(myBlock.transform.position, myLander.transform.position);
 
-        else if (distanceToTarget < 3)
-        {
-            Debug.Log("Wooooaaaah super close!!");
-            greenGlowRenderer.color = new Color(1f, 1f, 1f, 1f);
+            if (distanceToTarget < 3 && distanceToTarget > 1)
+            {
+                Debug.Log("You are close!");
+                greenGlowRenderer.color = new Color(1f, 1f, 1f, .5f);
+            }
+
+            else if (distanceToTarget < 1)
+            {
+                Debug.Log("Wooooaaaah super close!!");
+                greenGlowRenderer.color = new Color(1f, 1f, 1f, 1f);
+            }
         }
     }
 
