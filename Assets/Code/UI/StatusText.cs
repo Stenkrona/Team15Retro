40a8057e17a2fffb264a86fc6b110df7bf512203
ProfessorFFF <axelstenkrona@me.com>
2019-03-21 10:23:04 +0100

    .meta

---
+++ Assets/Code/UI/StatusText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusText : MonoBehaviour
+{
+
+    public bool amIPlayerOne;
+    
+    void Start()
+    {
+        if (InGameUIManager.GetInstance() != null)
+        {
+            if (amIPlayerOne)
+            {
+                if (InGameUIManager.GetInstance().PlayerOneInfo_Ref != null)
+                {
+                    InGameUIManager.GetInstance().PlayerOneInfo_Ref.playerStatusDisplay = GetComponent<Text>();
+                }
+                else
+                {
+                    Debug.Log("InGameUIManager is missing a ref to PlayerOneInfo_Ref");
+                }
+            }
+            else
+            {
+                InGameUIManager.GetInstance().PlayerTwoInfo_Ref.playerStatusDisplay = GetComponent<Text>();
+            }
+        }
+    }
+
+   
+}
