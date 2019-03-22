24377338f5b5dd7476f586c391505ab03457c75e
ProfessorFFF <axelstenkrona@me.com>
2019-03-22 11:52:01 +0100

    PlayerCharacter - Added more frames for the witches

--- Assets/Code/Player/PlayerCharacter.cs
+++ Assets/Code/Player/PlayerCharacter.cs
@@ -6,13 +6,45 @@ public class PlayerCharacter
 {
     
     private Sprite myPicture;
+    private Sprite[] mySprites;
+    
     private string myGamerTag;
     private string myIntroPhrase;
     private string myVictoryPhrase;
 
     public PlayerCharacter(Object[] portraits, Object[] introPhrase, Object[] victoryPhrase, Object[] gamerTag)
     {
-        myPicture = SetMyPicture(portraits);
+        
+        if (GameStateMachine.GetInstance().devMode)
+        {
+            myPicture = SetMyPicture(portraits);
+        }
+        else
+        {
+            if(portraits[0] != null)
+            {
+                mySprites = new Sprite[4];
+                myPicture = (Sprite)portraits[0];
+                mySprites[0] = (Sprite)portraits[0];
+                portraits[0] = null;
+
+                mySprites[1] = (Sprite)portraits[1];
+                mySprites[2] = (Sprite)portraits[2];
+                mySprites[3] = (Sprite)portraits[3];
+            }
+            else
+            {
+                mySprites = new Sprite[4];
+
+                myPicture = (Sprite)portraits[4];
+                mySprites[0] = (Sprite)portraits[4];                
+                portraits[4] = null;
+
+                mySprites[1] = (Sprite)portraits[5];
+                mySprites[2] = (Sprite)portraits[6];
+                mySprites[3] = (Sprite)portraits[7];
+            }
+        }
         myIntroPhrase = SetMyPhrase(introPhrase);
         myVictoryPhrase = SetMyPhrase(victoryPhrase);
         myGamerTag = SetMyPhrase(gamerTag);
@@ -64,6 +96,7 @@ public class PlayerCharacter
     public string MyIntroPhrase { get { return myIntroPhrase; } }
     public string MyVictoryPhrase { get { return myVictoryPhrase; } }
     public string MyGamerTag { get { return myGamerTag; } }
+    public Sprite[] MySprites { get { return mySprites; } }
 
 
 
